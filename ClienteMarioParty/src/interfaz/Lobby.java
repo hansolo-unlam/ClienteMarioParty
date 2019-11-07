cd9ed4cf8e0d52fccc325d3a2e56d2573da5d97d
LucasTeijo <54475564+LucasTeijo@users.noreply.github.com>
2019-11-07 17:42:02 -0300

    Add files via upload

---
+++ ClienteMarioParty/src/interfaz/Lobby.java
@@ -0,0 +1,132 @@
+package interfaz;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import cliente.Cliente;
+
+public class Lobby {
+
+	private final int WIDHT = 140;
+	private final int HEIGHT = 30;
+	private int desplazamiento = HEIGHT;
+	private int desplazamientoX = 0;
+
+	private JButton btnCrearSala;
+	private JFrame frame;
+	private JPanel contentPane;
+
+	private JLabel lblJugadores;
+	private JLabel lblSalas;
+	private JTextPane textPane;
+
+	private Cliente cliente;
+
+	private int cantSalas = 0;
+
+	public Lobby(String nombre) {
+		String nombreUser = JOptionPane.showInputDialog("Ingresar nombre");
+
+		this.cliente = new Cliente(nombreUser);
+		init(nombre);
+	}
+
+	private void init(String nombre) {
+		frame = new JFrame(nombre);
+		frame.setResizable(false);
+		frame.setSize(600, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblJugadores = new JLabel("JUGADORES EN LINEA");
+		lblJugadores.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblJugadores.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJugadores.setBounds(41, 22, 138, 32);
+
+		contentPane.add(lblJugadores);
+
+		lblSalas = new JLabel("SALAS");
+		lblSalas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSalas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSalas.setBounds(300, 22, 138, 32);
+
+		contentPane.add(lblSalas);
+
+		textPane = new JTextPane();
+		textPane.setBounds(25, 52, 180, 400);
+		textPane.setEditable(false);
+
+		contentPane.add(textPane);
+
+		btnCrearSala = new JButton("Crear nueva sala");
+		btnCrearSala.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String nombre = JOptionPane.showInputDialog("Ingresar nombre de la sala");
+				JsonObject jo = new JsonObject();
+				JsonObject jo1 = new JsonObject();
+				jo.addProperty("nombre", "NUEVA_SALA");
+				jo1.addProperty("nombreSala", nombre);
+				jo.add("data", jo1);
+				cliente.escribirMensaje(jo.toString());
+				Sala sala = new Sala(nombre, cliente);
+				JButton btnSala = new JButton(nombre);
+				cantSalas++;
+				if ((cantSalas - 1) % 7 == 0)
+					desplazamiento = 0;
+				if (cantSalas > 7) {
+					desplazamientoX = WIDHT * ((cantSalas - 1) / 7) + 30;
+				}
+				if (cantSalas == 14)
+					btnCrearSala.setEnabled(false);
+				textPane.setText(cantSalas + "\n");
+
+				btnSala.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("Ingresar a la sala");
+						JsonObject jo = new JsonObject();
+						JsonObject jo1 = new JsonObject();
+						jo.addProperty("nombre", "INGRESAR_SALA");
+						jo1.addProperty("nombreSala", nombre);
+						jo.add("data", jo1);
+						cliente.escribirMensaje(jo.toString());
+						Sala sala2 = new Sala(nombre, cliente);
+					}
+				});
+
+				btnSala.setBounds(240 + desplazamientoX, 60 + desplazamiento, WIDHT, HEIGHT);
+				desplazamiento += HEIGHT + 10;
+				contentPane.add(btnSala);
+				frame.repaint();
+			}
+
+		});
+
+		btnCrearSala.setBounds(241, 380, 160, 50);
+		contentPane.add(btnCrearSala);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Lobby("Lobby");
+	}
+
+}
---
+++ ClienteMarioParty/src/interfaz/Sala.java
@@ -0,0 +1,85 @@
+package interfaz;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+
+import com.google.gson.JsonObject;
+
+import cliente.Cliente;
+
+public class Sala {
+
+	private JFrame frame;
+	private int WIDTH = 410;
+	private int HEIGHT = 310;
+
+	private JLabel lblJugadores;
+	private JTextPane textPane;
+	private JButton btnSalirButton;
+	
+
+	public Sala(String nombre, Cliente cliente) {
+		init(nombre, cliente);
+		
+	}
+
+	private void init(String nombreSala, Cliente cliente) {
+		frame = new JFrame(nombreSala);
+		frame.setResizable(false);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+
+		frame.setLayout(null);
+
+		lblJugadores = new JLabel("JUGADORES");
+		lblJugadores.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblJugadores.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJugadores.setBounds(31, 22, 138, 32);
+		lblJugadores.setVisible(true);
+
+		frame.add(lblJugadores);
+
+		textPane = new JTextPane();
+		textPane.setBounds(41, 52, 138, 198);
+		textPane.setEditable(false);
+
+		frame.add(textPane);
+
+		btnSalirButton = new JButton("Salir de la sala");
+		btnSalirButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Tocaste salir");
+				JsonObject jo = new JsonObject();
+				JsonObject jo1 = new JsonObject();
+				jo.addProperty("nombre", "SALIR_SALA");
+				jo1.addProperty("nombreSala", nombreSala);
+				jo.add("data", jo1);
+				cliente.escribirMensaje(jo.toString());
+				frame.hide();
+			}
+		});
+		btnSalirButton.setBounds(241, 182, 120, 23);
+		frame.add(btnSalirButton);
+
+		JButton btnComenzarButton = new JButton("Comenzar");
+		btnComenzarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Empezar partida");
+			}
+		});
+		btnComenzarButton.setBounds(239, 216, 122, 23);
+		frame.add(btnComenzarButton);
+		frame.setVisible(true);
+	}
+
+
+}
